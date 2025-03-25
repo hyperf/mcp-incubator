a95b569b23858c166c38112c2aee62c09731e4e0
张城铭 <zhangchengming@addcn.com>
2025-03-25 10:09:46 +0800

    init

---
+++ src/ConfigProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Mcp;
+
+use Hyperf\Contract\IdGeneratorInterface;
+use Hyperf\Rpc\IdGenerator\UniqidIdGenerator;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+                IdGeneratorInterface::class => UniqidIdGenerator::class
+            ],
+        ];
+    }
+}
